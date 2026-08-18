aecafb1699fee40d806553202c4e1208d535a47c
Quoc <quocssanh@gmail.com>
2026-08-18 17:28:37 +0700

    Fix duplicate export in manager/index.tsx

--- fe/src/views/manager/index.tsx
+++ fe/src/views/manager/index.tsx
@@ -10,7 +10,3 @@ export { CRMManagement } from "./CRMManagement/index";
 export { InvoiceManagement } from "./InvoiceManagement/index";
 export { InventoryControl } from "../chef/inventory/index";
 export { QRCodeSettings } from "./QRCodeSettings/index";
-
-export { QRCodeSettings } from "./QRCodeSettings/index";
-
-
